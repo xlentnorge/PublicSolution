xlentnorge/TryggvePublic

Add quotes of the moment to homepage
Ref #5

PublicHomePage/Controllers/HomeController.cs
@@ -1,25 +1,27 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using PublicHomePage.Clients;
 using PublicHomePage.Models;
+using PublicHomePage.Models.ViewModels;
+using PublicHomePage.Providers;
 
 namespace PublicHomePage.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
-    private readonly IQuoteClient _quoteClient;
+    private readonly IQuoteProvider _quoteProvider;
 
-    public HomeController(ILogger<HomeController> logger, IQuoteClient quoteClient)
+    public HomeController(ILogger<HomeController> logger, IQuoteProvider quoteProvider)
     {
         _logger = logger;
-        _quoteClient = quoteClient;
+        _quoteProvider = quoteProvider;
     }
 
     public async Task<IActionResult> Index()
     {
-        var quotes = await _quoteClient.GetQuotes();
-        return View();
+        var quote = await _quoteProvider.QuoteOfTheDay();
+        var viewModel = new HomeModel(quote);
+        return View(viewModel);
     }
 
     public IActionResult Privacy()

PublicHomePage/Providers/IQuoteProvider.cs
@@ -1,0 +1,8 @@
+using PublicHomePage.Models.QuoteClient;
+
+namespace PublicHomePage.Providers;
+
+public interface IQuoteProvider
+{
+   public Task<Quote> QuoteOfTheDay();
+}

PublicHomePage/Providers/QuoteProvider.cs
@@ -1,0 +1,23 @@
+using PublicHomePage.Clients;
+using PublicHomePage.Models.QuoteClient;
+
+namespace PublicHomePage.Providers;
+
+public class QuoteProvider : IQuoteProvider
+{
+    private readonly IQuotesFreeApiClient _quotesFreeApiClient;
+
+    public QuoteProvider(IQuotesFreeApiClient quotesFreeApiClient)
+    {
+        _quotesFreeApiClient = quotesFreeApiClient;
+    }
+
+    public async Task<Quote> QuoteOfTheDay()
+    {
+        var quotes = await _quotesFreeApiClient.GetQuotes();
+        var random = new Random();
+        var enumerable = quotes.ToList();
+        var index = random.Next(0, enumerable.Count());
+        return enumerable.ElementAt(index);
+    }
+}

TryggvePublicTest/Providers/QuoteProviderTests.cs
@@ -1,0 +1,43 @@
+using FakeItEasy;
+using FluentAssertions;
+using PublicHomePage.Clients;
+using PublicHomePage.Models.QuoteClient;
+using PublicHomePage.Providers;
+
+namespace TryggvePublicTest.Providers;
+
+public class QuoteProviderTests
+{
+    private readonly IQuotesFreeApiClient _quotesFreeApiClientFake;
+    private readonly IQuoteProvider _quoteProvider;
+
+    public QuoteProviderTests()
+    {
+        _quotesFreeApiClientFake = A.Fake<IQuotesFreeApiClient>();
+        _quoteProvider = new QuoteProvider(_quotesFreeApiClientFake);
+    }
+
+    [Fact]
+    public async void TestQuoteOftheDay()
+    {
+        // Arrange
+        var quotes = new List<Quote>()
+        {
+            new()
+            {
+                Text = "FakeItEasy is fun",
+                Author = "My, Myself and I"
+            }
+        };
+        A.CallTo(() => _quotesFreeApiClientFake.GetQuotes()).Returns(quotes);
+
+
+        // Act
+        var result = await _quoteProvider.QuoteOfTheDay();
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Should().BeEquivalentTo(quotes.FirstOrDefault());
+        A.CallTo(() => _quotesFreeApiClientFake.GetQuotes()).MustHaveHappenedOnceExactly();
+    }
+}

xlentnorge/PublicSolution

PublicHomePage/Controllers/HomeController.cs
@@ -1,25 +1,27 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using PublicHomePage.Clients;
 using PublicHomePage.Models;
+using PublicHomePage.Models.ViewModels;
+using PublicHomePage.Providers;
 
 namespace PublicHomePage.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
-    private readonly IQuoteClient _quoteClient;
+    private readonly IQuoteProvider _quoteProvider;
 
-    public HomeController(ILogger<HomeController> logger, IQuoteClient quoteClient)
+    public HomeController(ILogger<HomeController> logger, IQuoteProvider quoteProvider)
     {
         _logger = logger;
-        _quoteClient = quoteClient;
+        _quoteProvider = quoteProvider;
     }
 
     public async Task<IActionResult> Index()
     {
-        var quotes = await _quoteClient.GetQuotes();
-        return View();
+        var quote = await _quoteProvider.GetRandomQuote();
+        var viewModel = new HomeModel(quote);
+        return View(viewModel);
     }
 
     public IActionResult Privacy()

PublicHomePage/Providers/IQuoteProvider.cs
@@ -1,0 +1,8 @@
+using PublicHomePage.Models.QuoteClient;
+
+namespace PublicHomePage.Providers;
+
+public interface IQuoteProvider
+{
+   public Task<Quote> GetRandomQuote();
+}

PublicHomePage/Providers/QuoteProvider.cs
@@ -1,0 +1,23 @@
+using PublicHomePage.Clients;
+using PublicHomePage.Models.QuoteClient;
+
+namespace PublicHomePage.Providers;
+
+public class QuoteProvider : IQuoteProvider
+{
+    private readonly IQuotesFreeApiClient _quotesFreeApiClient;
+
+    public QuoteProvider(IQuotesFreeApiClient quotesFreeApiClient)
+    {
+        _quotesFreeApiClient = quotesFreeApiClient;
+    }
+
+    public async Task<Quote> GetRandomQuote()
+    {
+        var quotes = await _quotesFreeApiClient.GetQuotes();
+        var random = new Random();
+        var list = quotes.ToList();
+        var index = random.Next(0, list.Count());
+        return list.ElementAt(index);
+    }
+}

TryggvePublicTest/Providers/QuoteProviderTests.cs
@@ -1,0 +1,43 @@
+using FakeItEasy;
+using FluentAssertions;
+using PublicHomePage.Clients;
+using PublicHomePage.Models.QuoteClient;
+using PublicHomePage.Providers;
+
+namespace TryggvePublicTest.Providers;
+
+public class QuoteProviderTests
+{
+    private readonly IQuotesFreeApiClient _quotesFreeApiClientFake;
+    private readonly IQuoteProvider _quoteProvider;
+
+    public QuoteProviderTests()
+    {
+        _quotesFreeApiClientFake = A.Fake<IQuotesFreeApiClient>();
+        _quoteProvider = new QuoteProvider(_quotesFreeApiClientFake);
+    }
+
+    [Fact]
+    public async void TestQuoteOftheDay()
+    {
+        // Arrange
+        var quotes = new List<Quote>()
+        {
+            new()
+            {
+                Text = "FakeItEasy is fun",
+                Author = "My, Myself and I"
+            }
+        };
+        A.CallTo(() => _quotesFreeApiClientFake.GetQuotes()).Returns(quotes);
+
+
+        // Act
+        var result = await _quoteProvider.GetRandomQuote();
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Should().BeEquivalentTo(quotes.FirstOrDefault());
+        A.CallTo(() => _quotesFreeApiClientFake.GetQuotes()).MustHaveHappenedOnceExactly();
+    }
+}